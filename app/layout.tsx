422c78aed277ad07cf0e0adef3d80855aeaeb7bc
i001962@gmail.com <i001962@gmail.com>
2024-02-04 22:30:06 -0800

    add title tage for wc

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>FC Chat Roulette (ish)</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body>{children}</body>
     </html>
   );
